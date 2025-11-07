4bc60d8a2013e0f6925948f3b2cca7cde5768e6f
marto-45 <martin.bra1029@gmail.com>
2025-11-06 19:21:27 -0500

    fotos-tabla

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/photos', async function (req, res, next) {
+  const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json'
+  const config = {
+    proxy: {
+      host: 'localhost',
+      port: 4444
+    }
+  }
+  const response = await axios.get(URL); //de ser necesario, incluya la constante config como segundo parámetro del método get
+  res.render('fotos', { title: 'Fotos', fotos: response.data });
+})
 module.exports = router;
